05619089cad205a332ad2a135d6807adca8f1022
MatyasHroch <matyas.hroch01@upol.cz>
2024-08-06 01:26:37 +0200

    full init

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export default {
+  main: {
+    langueage: "cs",
+    contact: "Kontakt",
+    gigs: "Koncerty",
+    news: "Novinky",
+    about: "Kapela",
+    records: "Nahrávky",
+    contact_page: {
+      title: "Kontakt",
+    },
+    about_page: {
+      title: "Kapela",
+    },
+  },
+};
